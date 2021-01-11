28a6d00e06fd8b485dc0ab36fcd4b938237251d8
thuqnn <phamthithuqn92@gmail.com>
2021-01-12 00:21:28 +0700

    okkk up

--- src/components/CompleteTask.js
+++ src/components/CompleteTask.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { DownOutlined, StarOutlined } from "@ant-design/icons";
 import classes from "./CompleteTask.module.css";
 
 function CompleteTask({ completeItems, maskTaskUncompleted, checked }) {
+  const [hide, setHide] = useState(true);
+  const onHandleShowHide = () => {
+    setHide(!hide);
+  };
   return (
     <div className={classes.completed}>
       <section className={classes.listCompleted}>
         <div className={classes.totalComplete}>
-          <DownOutlined />
+          <DownOutlined onClick={onHandleShowHide} />
           <span> Completed </span>
           <span>{completeItems.length}</span>
         </div>
         <ul>
           {completeItems.map((task) => {
             return (
-              <li key={task.id}>
+              <li key={task.id} className={hide ? classes.hide : classes.show}>
                 <div className={classes.wrapItem}>
                   <div className={classes.wrap}>
                     <input
